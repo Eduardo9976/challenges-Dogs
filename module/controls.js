391396218dc15f2de63435a8c3aec8ad7f4ee83b
Eduardo Fernandes <eduardo_2407@me.com>
2020-07-23 14:13:01 -0300

    Ajustando o JS

--- module/controls.js
+++ module/controls.js
@@ -1,18 +1,25 @@
 export default class Controls {
-  constructor(controles, tag) {
+  constructor(controles, tag, buttonSave, onSave) {
     this.controles = document.getElementById(controles);
-    this.seletor = document.querySelector(tag)
+    this.seletor = document.querySelector(tag);
+    this.buttonSave = document.querySelector(buttonSave)
+    this.onSave = document.querySelector(onSave)
+    this.styles = new Object()
 
     this.handleChange = this.handleChange.bind(this);
-    this.handleStyle = this.handleStyle.bind(this)
+    this.handleClickStyle = this.handleClickStyle.bind(this)
   }
   changeControls() {
     this.controles.addEventListener('change', this.handleChange);
+    this.buttonSave.addEventListener('click', this.handleClickStyle)
   }
 
   handleChange(event) {
-    this.handleStyle(event.target.name, event.target.value)
-    this.saveValues(event.target.name, event.target.value)
+    this.handleStyle(event.target.name, event.target.value) 
+    let name = event.target.name
+    let value = event.target.value
+    this.styles[`${name}`] = value
+    this.styles.temps = new Date().toLocaleString();
   }
 
   handleStyle(name, value) {
@@ -26,22 +33,31 @@ export default class Controls {
     };
   }
 
-  saveValues(name, value) {
-    localStorage.setItem(name, value)
+  handleClickStyle(event) {
+    event.preventDefault()
+    this.onSave.classList.add('on')
+    window.localStorage.dogs = JSON.stringify(this.styles)
+    setTimeout(() =>{
+      this.onSave.classList.remove('on')
+    }, 3000)
   }
 
   setValues() {
-    const keys = ["color", "backgroundColor", "fontFamily"]
-    keys.forEach((item) => {
-      this.handleStyle(item, localStorage.getItem(`${item}`))
-    })
+    if (window.localStorage.dogs){
+      this.styles = JSON.parse(window.localStorage.dogs)
+      const dogsStyles = JSON.parse(localStorage.getItem("dogs"))
+    
+      for (let prop in dogsStyles) {
+        this.handleStyle(prop, dogsStyles[prop])
+      };
+    }
   }
 
   init() {
-    if (this.controles && this.seletor) {
+    if (this.controles && this.seletor && this.buttonSave && this.onSave) {
       this.changeControls()
       this.setValues()
     };
   }
-  
 }
+
--- script.js
+++ script.js
@@ -3,6 +3,5 @@ import Controls from "./module/controls.js";
 
 const solution = new Solution("#dogs", ".image", ".searchField", ".search",    ".load", "https://dog.ceo/api/breeds/list/all").init();
 
-const controls = new Controls('controles', 'h1').init();
+const controls = new Controls('controles', 'h2', '.save', '.onSave').init();
 
-const button = new Controls('controlesButton', 'button').init();
